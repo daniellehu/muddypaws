b7761b84625e91b184a12e610d7870505fae48d8
daniellehu <daniellh@andrew.cmu.edu>
2017-10-15 23:42:31 -0400

    fixing CSS collision issues and Header className

--- muddypaws/src/components/Header.js
+++ muddypaws/src/components/Header.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 import cart from '../img/shopping-cart.svg';
 
@@ -8,19 +7,15 @@ import '../css/common.css';
 const Header = function(props) {
     return (
         <header>
-        <h2><a href="#" onClick={props.goHome}>Muddy Paws</a></h2>
-        <a href="#" onClick={props.goShoppingCart}>
-            <div class="shopping-cart">
+        <h2><a onClick={props.goHome}>Muddy Paws</a></h2>
+        <a onClick={props.goShoppingCart}>
+            <div className="shopping-cart">
                 <p id="cart-count">0</p>
-                <img id="cart" src={cart} />
+                <img id="cart" src={cart} alt="shopping cart" />
             </div>
         </a>
         </header>
     );
 };
-
-Header.propTypes = {
-    site: PropTypes.string.isRequired,
-}
 
 export default Header;
